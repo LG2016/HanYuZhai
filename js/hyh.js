240c195924f3191590318a8056b1f503de506f50
LiGuo <2216643277@qq.com>
2017-09-17 00:36:07 +0800

    add

---
+++ js/hyh.js
@@ -0,0 +1,24 @@
+(function($) {
+
+	$(function() {
+
+		$("#addPic").on("click", function(){
+			console.log("test");
+		});
+	
+
+			// $('.gallery').poptrox({
+			// 	useBodyOverflow: false,
+			// 	usePopupEasyClose: false,
+			// 	overlayColor: '#0a1919',
+			// 	overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+			// 	usePopupDefaultStyling: false,
+			// 	usePopupCaption: true,
+			// 	popupLoaderText: '',
+			// 	windowMargin: 10,
+			// 	usePopupNav: true
+			// });
+
+	});
+
+})(jQuery);
