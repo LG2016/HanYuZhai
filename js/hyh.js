15eab1834219707e238649fd507d280a12bd209c
LiGuo <2216643277@qq.com>
2017-10-08 22:17:51 +0800

    add

--- js/hyh.js
+++ js/hyh.js
@@ -1,24 +1,32 @@
 (function($) {
 
 	$(function() {
-
 		$("#addPic").on("click", function(){
-			console.log("test");
+			var min=$('#min').val();
+			var pic=$('#pic').val();
+			var des=$('#des').val();
+			addPicUrl(min,pic,des);
 		});
-	
-
-			// $('.gallery').poptrox({
-			// 	useBodyOverflow: false,
-			// 	usePopupEasyClose: false,
-			// 	overlayColor: '#0a1919',
-			// 	overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-			// 	usePopupDefaultStyling: false,
-			// 	usePopupCaption: true,
-			// 	popupLoaderText: '',
-			// 	windowMargin: 10,
-			// 	usePopupNav: true
-			// });
 
 	});
 
+ $(function(){
+ 	// alert("showPic");
+ });
+
 })(jQuery);
+
+function addPicUrl(min,pic,des){
+	AV.initialize('lvGBqWMwDN3NwEWDG1LmfgCK-gzGzoHsz', 'xFm44Y3iEY51P65afvqNjypq');
+	var Photo = AV.Object.extend('Photo');
+	var photo = new Photo();
+	photo.set('min',min);
+	photo.set('pic',pic);
+	photo.set('des',des);
+	photo.save().then(function (todo) {
+    console.log('objectId is ' + todo.id);
+  }, function (error) {
+    console.error(error);
+    alert("图片添加错误，重新添加");
+  });
+}
